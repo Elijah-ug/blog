97a3210abd55638c42d2ca143ea93243249b62c8
Elijah-ug <elijahtalentmugisha14@gmail.com>
2025-07-24 02:41:17 +0300

    backend routes mapped to the db

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,9 +1,11 @@
 import { query } from "../db/pool"
-import { createBlogQuery, readAllBlogsQuery } from "../db/sqlDatabaseQuery"
-import { createError } from "./utils/error";
+import {
+    createBlogQuery, deleteBlogQuery, readAllBlogsQuery, readSingleBlogQuery, updateBlogQuery
+} from "../db/sqlDatabaseQuery";
+import { createError } from "../utils/error";
 // create blog controller
 export const createBlogController = async (rq, res, next) => {
-    const { title, tag, body, thumbnail } = req.body();
+    const { title, tag, body, thumbnail } = req.body;
     try {
         if (!title || !tag || !body || !thumbnail) {
             return res.status(400).json("Missing Fields!");
@@ -24,6 +26,53 @@ export const readAllBlogsController = async (req, res, next) => {
     } catch (error) {
         console.log(error);
         next(createError(400, "Failed to fetch blogs"));
+    }
+}
+//
+// Read
+export const readSingleBlogController = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const data = await query(readSingleBlogQuery, [id]);
+        if (!data.rows.length) {
+            return next(createError(400, "Blog not found"));
+        }
+        res.status(200).json(data.rows);
+    } catch (error) {
+        console.log(error);
+        next(createError(400, "Failed to fetch blogs"));
+    }
+}
+
+// update
+export const updateSingleBlogController = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { title, tag, body, thumbnail } = req.body;
+        const data = await query(updateBlogQuery, [title, tag, body, thumbnail, id]);
+        if (!data.rowCount) {
+            return next(createError(400, "Blog not found"));
+        }
+        console.log(data);
+        res.status(200).json(data.rows);
+    } catch (error) {
+        console.log(error);
+        next(createError(400, "Failed to Update blog"));
+    }
+}
+
+export const deleteBlogController = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const data = await query(deleteBlogQuery, [id]);
+        if (!id) {
+            return next(createError(400, "Blog not found"));
+        }
+        console.log(data);
+        res.status(200).json({message: "Blog deleted"});
+    } catch (error) {
+        console.log(error);
+        next(createError(500, "Failed to Delete blog"));
     }
 }
 //
---
+++ backend/routes/posts.js
@@ -0,0 +1,11 @@
+import express from "express";
+import {
+    createBlogController, deleteBlogController, readAllBlogsController, readSingleBlogController, updateSingleBlogController
+} from "../controllers/postController";
+const router = express.Router();
+
+router.get("/", readAllBlogsController);
+router.get("/:id", readSingleBlogController);
+router.post("/", createBlogController);
+router.put("/:id", updateSingleBlogController);
+router.delete("/:id", deleteBlogController);
